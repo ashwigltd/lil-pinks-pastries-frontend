4d1f83cc752ccf7741184fd2d321642c78cd023a
Ash Hellwig <ahellwig.dev@gmail.com>
2021-07-29 18:57:14 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Lil Pink's Pastries",
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-theme-ui"],
+};
