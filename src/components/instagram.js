119681be676902ae8377c4416b514c62e15a1a33
Ash Hellwig <ash@ashwigltd.com>
2021-11-15 00:17:16 -0700

    THE GALLERY WORKS

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,35 +1,75 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
 
-const InstagramGallery = () => (
-  <StaticQuery
-    query={graphql`
-      {
-        allInstaNode {
-          edges {
-            node {
-              id
-              likes
-              comments
-              mediaType
-              localFile {
-                publicURL
+import Gallery from "@browniebroke/gatsby-image-gallery"
+
+// const InstagramGallery = ({ data }) => {
+// const images = data.allInstaNode.edges.map(
+//   ({ node }) => node.localFile.childImageSharp
+// )
+// `images` is an array of objects with `thumb` and `full`
+// return <Gallery images={images} />
+// }
+
+// export const pageQuery = graphql`
+//   query InstagramGalleryQuery {
+//     allInstaNode {
+//       edges {
+//         node {
+//           localFile {
+//             absolutePath
+//             childImageSharp {
+//               thumb: gatsbyImageData(
+//                 width: 270
+//                 height: 270
+//                 placeholder: BLURRED
+//               )
+//               full: gatsbyImageData(layout: FULL_WIDTH)
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+// export default InstagramGallery
+
+const InstagramGallery = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allInstaNode {
+        edges {
+          node {
+            localFile {
+              absolutePath
+              childImageSharp {
+                thumb: gatsbyImageData(
+                  width: 270
+                  height: 270
+                  placeholder: BLURRED
+                )
+                full: gatsbyImageData(layout: FULL_WIDTH)
               }
             }
           }
         }
       }
-    `}
-    render={data => (
+    }
+  `)
+
+  const images = data.allInstaNode.edges.map(
+    ({ node }) => node.localFile.childImageSharp
+  )
+
+  return (
+    <div>
+      <h2>Instagram Gallery</h2>
       <div>
-        <div>
-          {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
-          <h2>Instagram Feed</h2>
-          <p>{data.allInstaNode.edges}</p>
-        </div>
+        <Gallery images={images}></Gallery>
       </div>
-    )}
-  ></StaticQuery>
-)
+    </div>
+  )
+}
 
 export default InstagramGallery
