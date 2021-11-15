36ecf8b5ce9e3c3b1ebc18a27ee10c047086864d
Ash Hellwig <ahellwig.dev@gmail.com>
2021-11-14 22:59:03 -0700

    insta

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,27 +1,35 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-const InstagramGallery = ({ data }) => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query InstagramGalleryQuery {
-          allInstaNode(filter: { mediaType: { eq: "IMAGE" } }) {
-            edges {
-              node {
-                id
-                }
+const InstagramGallery = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        allInstaNode {
+          edges {
+            node {
+              id
+              likes
+              comments
+              mediaType
+              localFile {
+                publicURL
               }
             }
           }
         }
-      `}
-
-      render={data => (
+      }
+    `}
+    render={data => (
+      <div>
         <div>
+          {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
           <h2>Instagram Feed</h2>
-          <p>{data.allInstaNode.edges.node.permalink}</p>
+          <p>{data.allInstaNode.edges}</p>
         </div>
-      });
-    } />
+      </div>
+    )}
+  ></StaticQuery>
+)
 
+export default InstagramGallery
